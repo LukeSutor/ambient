22d76636e8e8d25181fdb5383f562ebe5b08b363
Luke Sutor <lukesutor@gmail.com>
2025-01-01 23:15:36 -0500

    remove llama.cpp rust bindings

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod llm;
-use llm::LLMService;
-use tauri::Manager;
-use async_std::sync::Mutex;
-
-#[tauri::command]
-async fn generate_text(state: tauri::State<'_, Mutex<LLMService>>, prompt: String) -> Result<String, String> {
-    let state = state.lock().await;
-    state.generate(prompt).await
-}
-
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let llm_service = LLMService::new("llm/models/llava-v1.6-mistral-7b.Q4_K_M.gguf".into())
-                .map_err(|e| e.to_string())?;
-            app.manage(Mutex::new(llm_service));
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![generate_text])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,10 @@ import "./App.css";
 
 function App() {
 
-  async function gen(p) {
-    // Generate text
-    const response = await invoke('generate_text', { 
-      prompt: p
-    })
-    console.log(response)
-  }
-
-  // WebSocket setup
-  useEffect(() => {
-// Generate text
-    gen("hello world in python is what");
-  }, []);
-
-
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
       <div className="">
-        <button onClick={() => gen("what's your favorite color")}>Click me</button>
       </div>
     </main>
   );
