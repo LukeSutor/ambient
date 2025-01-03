7909872525d73eb041e1e5487f10efbe055aa56d
Luke Sutor <lukesutor@gmail.com>
2025-01-03 00:34:11 -0500

    renove unnecessary server files

--- src-tauri/llm/server/run-llm.bat
+++ src-tauri/llm/server/run-llm.bat
@@ -5,4 +5,4 @@ set IMAGE_PATH=C:\Users\Luke\Desktop\coding\local-computer-use\src-tauri\icons\S
 set PROMPT="what is this image? Respond in JSON format with the value \"answer\""
 set SCHEMA="{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"title\":\"LLM Answer Schema\",\"description\":\"Schema for constraining LLM output to a single answer string\",\"type\":\"object\",\"properties\":{\"answer\":{\"type\":\"string\",\"description\":\"The LLM's response\"}},\"required\":[\"answer\"],\"additionalProperties\":false}"
 
-C:\Users\Luke\Desktop\coding\local-computer-use\src-tauri\llm\llama.cpp\build\bin\Release\llama-qwen2vl-cli -m %MODEL_PATH% --mmproj %MMPROJ_PATH% --image %IMAGE_PATH% -p %PROMPT% -j %SCHEMA%
+C:\Users\Luke\Desktop\coding\local-computer-use\src-tauri\llm\server\build\bin\Release\qwen2vl-server.exe -m %MODEL_PATH% --mmproj %MMPROJ_PATH% --image %IMAGE_PATH% -p %PROMPT% -j %SCHEMA%
